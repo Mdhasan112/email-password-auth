98c9618e6c6d258b636a8596efecbeaba27d55d8
hasan <mdhasanhk.98@gmail.com>
2022-10-18 02:27:05 +0600

    form

--- src/App.js
+++ src/App.js
@@ -1,77 +1,47 @@
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import app from "./firebase.init";
+import { getAuth } from "firebase/auth";
+import app from './firebase.init';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { useState } from "react";
 
 const auth = getAuth(app);
 function App() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('')
-  const [validated, setValidated] = useState(false);
-
-  const handleEmailBlur = (event) => {
-    setEmail(event.target.value);
-  };
-  const handlePassBlur = event => {
-    setPassword(event.target.value)
-  };
-
-  const handleFormSubmit = event => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-      return;
+    const handleEmailBlur = event => {
+        console.log(event.target.value)
+    };
+    const handlePasswordBlur = event => {
+        console.log(event.target.value)
+    };
+    const handleFormSubmit = event => {
+        console.log("submited")
+        event.preventDefault()
     }
+    return (
+        <div className='w-50 mx-auto mt-2'>
+            <h2 className='text-primary'>Please Register</h2>
+            <Form onSubmit={handleFormSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
 
-    setValidated(true);
-
-    createUserWithEmailAndPassword(auth, email, password)
-      .then(result => {
-        const user = result.user;
-        console.log(user);
-        setEmail('');
-        setPassword('')
-      })
-      .catch(error => {
-        console.log(error)
-      })
-    event.preventDefault()
-  };
-  return (
-    <div>
-      <div className="registration w-50 mx-auto mt-5">
-        <h2 className="text-primary py-3">Please Register!!</h2>
-        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
-            <Form.Control.Feedback type="invalid">
-              Please choose a email.
-            </Form.Control.Feedback>
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control onBlur={handlePassBlur} type="password" placeholder="Password" required />
-            <Form.Control.Feedback type="invalid">
-              Please choose a password.
-            </Form.Control.Feedback>
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Register
-          </Button>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
         </Form>
-      </div>
-
-    </div>
-  );
-}
+        </div>
+    );
+};
 
 export default App;
